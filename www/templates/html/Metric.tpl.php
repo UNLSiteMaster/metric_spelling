983489ffccf5a15792846688e1a128c622cd5629
Michael Fairchild <mfairchild365@gmail.com>
2017-06-13 10:14:10 -0500

    Add a link to the spelling documentation

--- www/templates/html/Metric.tpl.php
+++ www/templates/html/Metric.tpl.php
@@ -1,5 +1,5 @@
 <p>
-    This spell checking metric uses several sources including <a href="http://wordlist.aspell.net/">SCOWL</a>, <a href="http://www.wordnik.com/">wordnik</a>, and U.S. government name data. If there are any words that are incorrectly marked as an error, please create a site-wide override for it. When enough site-wide overrides have been created, the word will be added to a custom dictionary.
+    This spell checking metric uses several sources including <a href="http://wordlist.aspell.net/">SCOWL</a>, <a href="http://www.wordnik.com/">wordnik</a>, and U.S. government name data. If there are any words that are incorrectly marked as an error, please create a site-wide override for it. When enough site-wide overrides have been created, the word will be added to a custom dictionary. <a href="http://wdn.unl.edu/documentation/unl-webaudit/metric-spelling">View more documentation on the spelling metric</a>.
 </p>
 
 <?php
