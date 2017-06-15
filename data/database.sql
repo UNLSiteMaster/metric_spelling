ef09725aa18ceda0da753154269fbdb839f5d3cb
Michael Fairchild <mfairchild365@gmail.com>
2017-06-15 10:09:38 -0500

    Make the word column case sensitive

--- data/database.sql
+++ data/database.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `metric_spelling_wordnik_cache` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `word` VARCHAR(256) NOT NULL,
+  `word` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `date_created` DATETIME NOT NULL,
   `result` ENUM('OKAY', 'ERROR') NOT NULL,
   PRIMARY KEY (`id`),
